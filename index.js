c0e7acece1b51c62b6ce3037122bec6f8fbe922b
webdev498 <gcmobile@mail.com>
2015-10-31 10:10:14 -0600

    chore(middleware): Register default middlewares

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+import Application from './application';
+
 import health from './lib/health';
 import datastore from './initializers/datastore';
-import Application from './application';
+import logging from './initializers/logging';
+import contentNegotiation from './initializers/content-negotiation';
+import compression from './initializers/compression';
 
 export default {
   run: (port) => {
     let application = new Application();
+    // Register middleware
     application.initialize(datastore);
+    application.initialize(logging);
+    application.initialize(contentNegotiation);
+    application.initialize(compression);
+
+    // Register routes
     application.register(health);
+
+    // Bind and run
     application.bind(port);
   }
 }
