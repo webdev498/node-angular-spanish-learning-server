d50b7a9a55b222cfdfa1c5447164e65968cbcc59
webdev498 <gcmobile@mail.com>
2016-03-06 19:56:27 -0700

    refactor(Database Migrations): Move migrations to knex

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
