35dbd4f48b81a9fce10a6df6f3b5fe121abc92cb
webdev498 <gcmobile@mail.com>
2016-10-17 19:26:37 -0600

    Typed recipient as User

--- src/email/models/Message.js
+++ src/email/models/Message.js
@@ -1,5 +1,7 @@
+import type User from './../users/models/User';
+
 export default class Message {
-    constructor(recipient) {
+    constructor(recipient: User) {
       this.from = process.env.DEFAULT_FROM_ADDRESS;
       this.recipient = recipient;
       this.to = recipient.get('email');
