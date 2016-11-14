3d7592f649e0eb386b06a023af97b7d457ca456c
webdev498 <gcmobile@mail.com>
2016-11-13 18:37:59 -0700

    CGI Admin email

--- src/email/models/template/QuestionFeedback.js
+++ src/email/models/template/QuestionFeedback.js
@@ -7,24 +7,39 @@
       this.feedbackText = feedbackText;
       this.question = question;
       this.fromUser = fromUser;
-      this.to = 'consulting@surfdew.com';
-      //this.cc = fromUser.get('email');
-      //this.to = 'admin@commongroundinternational.com';
+      this.cc = fromUser.get('email');
+      this.to = 'admin@commongroundinternational.com';
     }
 
     get body() {
       let formattedQuestion = JSON.stringify(this.question,undefined,2); //pretty format the object
-      let part1 = `From ${this.fromUser.get('firstName')} ${this.fromUser.get('lastName')}: <p>${this.feedbackText}</p>Question:<p>${this.question.type} : ${this.question.question.text}</p>`;
-      let part2 = `${part1}<p>Choices:</p>`;
+      let intro = `From ${this.fromUser.get('firstName')} ${this.fromUser.get('lastName')}: <p>${this.feedbackText}</p>Question:<p>${this.question.type} : ${this.question.question.text}</p>`;
+      let termBody = `${intro}<p>Terms:</p>`;
 
       switch (this.question.type) {
         case 'Multiple Choice':
           for (let i = 0; i < this.question.choices.length; i++) {
-              part2 += `${this.question.choices[i].text}<br>`
+              termBody += `${this.question.choices[i].text}<br>`
+          }
+          break;
+        case 'Category Matching':
+          for (let i = 0; i < this.question.categories.length; i++) {
+              termBody += `${this.question.categories[i].text}<br>`
+          }
+
+          termBody += '<p></p>';
+
+          for (let i = 0; i < this.question.choices.length; i++) {
+              termBody += `${this.question.choices[i].text}<br>`
+          }
+          break;
+        case 'Term Matching':
+          for (let i = 0; i < this.question.terms.length; i++) {
+              termBody += `${this.question.terms[i].text}<br>`
           }
           break;
       }
 
-      return part2;
+      return termBody;
   }
 }
