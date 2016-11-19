f5781c372ddaaf453ab8d78b724da915e8df6f12
webdev498 <gcmobile@mail.com>
2016-11-19 16:17:14 -0700

    WIP - Handle when user requests resource that does not belong to them

--- src/examinations/controllers/ExaminationResultsController.js
+++ src/examinations/controllers/ExaminationResultsController.js
@@ -1,5 +1,6 @@
 //@flow
 import ExaminationResultService from 'examinations/services/ExaminationResultService';
+import UnauthorizedError from 'exceptions/requests/Unauthorized.js';
 import type { Request } from 'http/index';
 
 export default class ExaminationResultsController {
@@ -10,8 +11,12 @@ export default class ExaminationResultsController {
   }
 
   async latestForUser(request: Request, reply: Function) {
+    const { id } = request.params;
     const { credentials } = request.auth;
     try {
+      if (id !== credentials.get('id')) {
+        throw new UnauthorizedError(request, 'You do not have permission to request this users results');
+      }
       const results = await this.service.getResultsForUser(credentials, 1);
       reply(results);
     } catch (error) {
--- src/exceptions/globalHandler.js
+++ src/exceptions/globalHandler.js
@@ -5,6 +5,9 @@ export const register = (server, options, next) => {
     if (request.response.isBoom) { // reply with a slightly more user-friendly error message
       let serviceError = ServiceErrorFactory.create(request, request.response);
       reply(serviceError).statusCode = serviceError.statusCode;
+    } else if (request.response.source.isError) {
+      let serviceError = ServiceErrorFactory.create(request, request.response.source);
+      reply(serviceError).statusCode = serviceError.statusCode;
     } else {
       reply.continue();
     }
