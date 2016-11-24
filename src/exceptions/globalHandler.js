1725e2d0b72e2ee51d4f5938c1cf1734abb7ef96
webdev498 <gcmobile@mail.com>
2016-11-23 18:46:01 -0700

    fix(Exception Handling): Fix logic in global request handlers for options requsts

--- src/exceptions/globalHandler.js
+++ src/exceptions/globalHandler.js
@@ -5,7 +5,7 @@ export const register = (server, options, next) => {
     if (request.response.isBoom) { // reply with a slightly more user-friendly error message
       let serviceError = ServiceErrorFactory.create(request, request.response);
       reply(serviceError).statusCode = serviceError.statusCode;
-    } else if (request.response.source.isError) {
+    } else if (request.response.source && request.response.source.isError) {
       let serviceError = ServiceErrorFactory.create(request, request.response.source);
       reply(serviceError).statusCode = serviceError.statusCode;
     } else {
