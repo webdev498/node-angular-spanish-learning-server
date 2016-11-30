4691b08d6497ef0f3a77a0f6ce8064d403642b1b
webdev498 <gcmobile@mail.com>
2016-11-29 21:37:54 -0700

    Spanish and English diversification

--- src/examinations/strategies/grading/index.js
+++ src/examinations/strategies/grading/index.js
@@ -3,7 +3,8 @@ import TermMatching from './term-matching-strategy';
 import CategoryMatching from './category-matching-strategy';
 
 export default {
-  'Multiple Choice': MultipleChoice,
+  'Multiple Choice English': MultipleChoice,
+  'Multiple Choice Spanish': MultipleChoice,
   'Term Matching': TermMatching,
   'Category Matching': CategoryMatching
 };
--- src/examinations/templates/exam-template.js
+++ src/examinations/templates/exam-template.js
@@ -16,10 +16,16 @@ export type ExamSection = {
 export const sections = [
   {
     id: "65a605e4-791b-4db2-85d8-507bfe6cea20",
-    type: "Multiple Choice",
+    type: "Multiple Choice Spanish",
     instructions: "For the following questions choose the term the most correctly answers the question",
-    itemCount: (type: string) => Math.floor(itemCounts[type] * .50)
-  },
+    itemCount: type => Math.floor(itemCounts[type] * .25)
+  }, 
+  {
+    id: "c541034e-5e93-44d3-b05b-c569572a7f4a",
+    type: "Multiple Choice English",
+    instructions: "For the following questions choose the term the most correctly answers the question",
+    itemCount: type => Math.floor(itemCounts[type] * .25)
+  }, 
   {
     id: "ca4d8b1b-a554-4170-9ee2-99fe454d7061",
     type: "Term Matching",
