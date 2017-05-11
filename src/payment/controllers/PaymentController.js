3a74b16bb0c2c5755d801101b9176a621ea0c255
webdev498 <gcmobile@mail.com>
2017-05-10 19:20:49 -0600

    await subscription create

--- src/payment/controllers/PaymentController.js
+++ src/payment/controllers/PaymentController.js
@@ -40,7 +40,7 @@ export default class PaymentController {
       this.userService.changeRole(result.userId, role);
       //save paypal credentials
       const user = await this.userService.get({id: result.userId});
-      this.subscriptionService.create(user, 'study', result.agreement);
+      await this.subscriptionService.create(user, 'study', result.agreement);
       reply().statusCode = NO_CONTENT;
     } catch (error) {
       reply(error);
@@ -52,7 +52,7 @@ export default class PaymentController {
       const { credentials } = request.auth;
 
       //update paypal credentials
-      const user = await this.userService.get({id: credentials.userId});
+      const user = await this.userService.get({id: credentials.id});
       const subscription = user.related('subscription');
       const billingAgreement = subscription.get('billingAgreement');
 
