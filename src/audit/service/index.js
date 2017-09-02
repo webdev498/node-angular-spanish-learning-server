6ee297eed9b8a10ca13494c3df4067e03e24aa1b
webdev498 <gcmobile@mail.com>
2017-09-02 09:39:47 -0600

    uuid

--- src/audit/service/index.js
+++ src/audit/service/index.js
@@ -1,10 +1,11 @@
 import { logInfo } from './../../logging';
 import Audit from './models/Audit';
+const uuidv4 = require('uuid/v4');
 
 export default class AuditService {
     async log (userId, eventId) {
         const auditInfo = {
-            "id": "",
+            "id": uuidv4(),
             "eventId": eventId,
             "userId": userId
         };
